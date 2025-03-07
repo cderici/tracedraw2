ce8a7347e6b56308b24b4190f3929b58fe0308b9
Caner Derici <caner@dericilab.live>
2025-03-07 22:28:20 +0000

    feat(reader): grab jit summary section raw

--- internal/reader/reader.go
+++ internal/reader/reader.go
@@ -17,32 +17,32 @@ func IngestTraceFile(filename string) common.Trace {
 func IngestRaw(scanner bufio.Scanner) (common.TraceRaw, error) {
 	sections := make(map[string]*strings.Builder)
 
-	var currentSection *strings.Builder
+	var currentSectionBuilder *strings.Builder
 	var currentSectionName string
 
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
 
 		// check if line starts a new section
-		if strings.HasPrefix(line, "{") {
-			currentSectionName = strings.TrimPrefix(line, "{")
-			if currentSectionName != "" {
-				sections[currentSectionName] = &strings.Builder{}
-				currentSection = sections[currentSectionName]
-			}
+		if strings.Contains(line, "{"+JITSUMMARY_SECTION) {
+			currentSectionName = JITSUMMARY_SECTION
+			sections[currentSectionName] = &strings.Builder{}
+			currentSectionBuilder = sections[currentSectionName]
+
 			continue
 		}
 
 		// check if line ends the current section
-		if line == currentSectionName+"}" {
-			currentSection = nil
+		if strings.Contains(line, currentSectionName+"}") {
+			currentSectionBuilder = nil
 			currentSectionName = ""
+
 			continue
 		}
 
 		// if inside a secton, append to the corresponding builder
-		if currentSection != nil {
-			currentSection.WriteString(line + "\n")
+		if currentSectionBuilder != nil {
+			currentSectionBuilder.WriteString(line + "\n")
 		}
 	}
 
@@ -62,8 +62,13 @@ func IngestRaw(scanner bufio.Scanner) (common.TraceRaw, error) {
 
 func getSectionContents(sections map[string]*strings.Builder, sectionName string) (string, error) {
 	var sectionContents string
-	if _, exists := sections[sectionName]; !exists {
-		return "", je.Annotatef(common.ErrIngestNoSuchSection, "section %s", sectionName)
+	var err error
+
+	if contents, exists := sections[sectionName]; exists {
+		sectionContents = contents.String()
+	} else {
+		err = je.Annotatef(common.ErrIngestNoSuchSection, "section %s", sectionName)
 	}
-	return sectionContents, nil
+
+	return sectionContents, err
 }
