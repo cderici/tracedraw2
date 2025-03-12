56f450f2e631463dca2b93030fc9a2609db9f61a
Caner Derici <caner@dericilab.live>
2025-03-12 21:19:04 +0000

    feat(cli): start adding a cli client

---
+++ cmd/tracedraw2-cli/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cderici/tracedraw2/cmd/tracedraw2-cli/commands"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: tracedraw2-cli print-raw <tracefile>")
+		os.Exit(1)
+	}
+	cmd := os.Args[1]
+	args := os.Args[2:]
+
+	switch cmd {
+	case "print-raw":
+		commands.DoPrintRaw(args)
+	default:
+		fmt.Printf("Unknown command: %q\n", cmd)
+		os.Exit(1)
+	}
+}
