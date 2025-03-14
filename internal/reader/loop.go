a0efe6e636089932ce16901ad9e236e2c45a4cf9
Caner Derici <caner@dericilab.live>
2025-03-14 04:36:54 +0000

    feat(reader): start adding loop reader

---
+++ internal/reader/loop.go
@@ -0,0 +1,12 @@
+package reader
+
+type LoopRaw struct {
+	PreambleId   string
+	InnerLoopId  string
+	PreambleOps  []string
+	InnerLoopOps []string
+}
+
+func readLoop(loopRaw string) LoopRaw {
+
+}
